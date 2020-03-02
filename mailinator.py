854df19b68417a7e9340095d8a9591b03771b80e
Guneet Singh <guneetsinghbali@gmail.com>
2020-03-02 22:52:53 +0530

    auto created emails

--- mailinator.py
+++ mailinator.py
@@ -3,6 +3,7 @@ from time import sleep
 import time
 import unicodedata
 import requests
+from random_username.generate import generate_username
 from selenium.common.exceptions import TimeoutException
 
 driver = webdriver.Chrome(executable_path="C://Users//gunee//Desktop//TinderBot//chromedriver.exe")
@@ -12,15 +13,17 @@ driver.get("https://www.mailinator.com/v3/#/#inboxpane")
 driver.implicitly_wait(10)
 
 def main():
-
+    random = int(input('Enter random text you want:'))
+    file1 = open("usr.csv","w")
+    file2 = generate_username(random)
+    str1 = '\n'.join(map(str, file2))
+    file1.write(str1)
+    print(str1)
+    file1.close()
     with open('usr.csv','r') as usr:
       while (True):
         username = usr.readline()
-        cnt = 1
-        # for name in username:
-        print("Line {}: {}".format(cnt, username.strip()))
-            # name = usr.readline()
-        cnt += 1
+        print("Line: {}".format(username.strip()))       
         if username == "":
             print("::DONE::")
             break
@@ -28,21 +31,22 @@ def main():
         if username == "\n":
             print("::EMPTY LINE::")
             continue
-        driver.implicitly_wait(20)      
+        driver.implicitly_wait(10)      
         email = driver.find_element_by_xpath('//*[@id="inbox_field"]')
         slow_typing(email,username)
         # driver.save_screenshot('image.png')
         driver.implicitly_wait(10)
         go = driver.find_element_by_xpath('//*[@id="go_inbox"]')            
         go.click()
+        
+        driver.find_element_by_xpath('//*[@id="inbox_field"]').clear()
         driver.implicitly_wait(10)
-        email.clear()
-      print(cnt)    
+         
 
 def slow_typing(element, text):
    for character in text:
       element.send_keys(character)
-      time.sleep(0.3)        
+      time.sleep(0.5)        
 
 if __name__ == '__main__':
    main()
